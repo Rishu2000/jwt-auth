71cb19fdbc463d6a0ba83f43b827dbde68df6a36
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-10 10:11:56 +0530

    basic get api to get all the posts.

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Rishav kumar");
+const express = require('express');
+const app = express();
+const port = 3003;
+
+const posts = [
+    {
+        username:'Rishav',
+        post:'Post 1'
+    },{
+        username:'Kumar',
+        post:'Post 2'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
+})
+
+app.listen(port,() => {
+    console.log('server is listning to the port '+port);
+})
