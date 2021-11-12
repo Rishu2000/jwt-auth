9488fc55ea2549e217af4b93f594a860e1a7cefc
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-12 19:43:42 +0530

    Imported .env file and created the access token using "jwt.sign()".

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const port = 3003;
 
 app.use(express.json());
@@ -37,19 +39,21 @@ app.post('/user', async (req, res) => {
 })
 
 app.post('/user/login', async (req, res) => {
-    const user = users.find(user => user.username === req.body.username);
+    const user = users.find(item => item.username === req.body.username);
     if(user == null){
         res.status(400).json("Incorrect username and password.");
-    }
-    try {
-        if(await bcrypt.compare(req.body.password, user.password)){
-            res.json("Successfully loged in.");
-        }else{
-            res.status.json("Password is incorrect.");
+    }else{
+        try {
+            if(await bcrypt.compare(req.body.password, user.password)){
+                const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+                res.json(accessToken);
+            }else{
+                res.status.json("Password is incorrect.");
+            }
         }
-    }
-    catch {
-        res.status(500).json("Error");
+        catch(err) {
+            res.status(500).json(err);
+        }
     }
 })
 
