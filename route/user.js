06d2bdd73e158204ec7603421b7d4520feef0e78
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-12 20:23:05 +0530

    Implemented moduler routing for user module.

---
+++ route/user.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const users = [];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
+        const user = {username:req.body.username, password:hashedPassword};
+        users.push(user);
+        res.json("Created");
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const user = users.find(item => item.username === req.body.username);
+    if(user == null){
+        res.status(400).json("Incorrect username and password.");
+    }else{
+        try {
+            if(await bcrypt.compare(req.body.password, user.password)){
+                const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+                res.json(accessToken);
+            }else{
+                res.status.json("Password is incorrect.");
+            }
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    }
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,51 +1,13 @@
 const express = require('express');
 const app = express();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
 const posts = require('./route/posts');
+const user = require('./route/user');
 const port = 3003;
 
 app.use(express.json());
 
-const users = [];
-
 app.use('/posts',posts);
-
-app.get('/users', (req, res) => {
-    res.json(users);
-})
-
-app.post('/user', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password,10);
-        const user = {username:req.body.username, password:hashedPassword};
-        users.push(user);
-        res.json("Created");
-    }
-    catch {
-        res.status(500).json("Error");
-    }
-})
-
-app.post('/user/login', async (req, res) => {
-    const user = users.find(item => item.username === req.body.username);
-    if(user == null){
-        res.status(400).json("Incorrect username and password.");
-    }else{
-        try {
-            if(await bcrypt.compare(req.body.password, user.password)){
-                const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
-                res.json(accessToken);
-            }else{
-                res.status.json("Password is incorrect.");
-            }
-        }
-        catch(err) {
-            res.status(500).json(err);
-        }
-    }
-})
+app.use('/user',user);
 
 app.listen(port,() => {
     console.log('server is listning to the port '+port);
