fb228efb8b80604402e6da3b891319f90bb36fc8
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-13 18:29:36 +0530

    Created a call to get a new accessToken after the previous one expires and made it validate it only for 20sec.

--- route/user.js
+++ route/user.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const users = [];
+let refToken = "";
 
 app.get('/users', (req, res) => {
     res.json(users);
@@ -30,7 +31,9 @@ app.post('/login', async (req, res) => {
         try {
             if(await bcrypt.compare(req.body.password, user.password)){
                 const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'30s'});
-                res.json(accessToken);
+                const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET);
+                refToken = refreshToken;
+                res.json({accessToken, refToken});
             }else{
                 res.status.json("Password is incorrect.");
             }
@@ -39,6 +42,18 @@ app.post('/login', async (req, res) => {
             res.status(500).json(err);
         }
     }
+})
+
+app.post('/token',(req,res) => {
+    const refreshToken = req.body.refreshToken;
+    if(!refreshToken) return res.status(403).json("No Refresh token passed");
+    if(!(refreshToken === refToken)) return res.status(403).json("Invalid Refresh token passed");
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET, (err, item) => {
+        if(err) return res.status(401).json(err);
+        delete item.iat;
+        const accessToken = jwt.sign(item,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'20s'});
+        res.json(accessToken);
+    })
 })
 
 module.exports = app;
